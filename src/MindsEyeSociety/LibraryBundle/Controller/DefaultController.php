453ae19a5938e174a2648c309be91fcd7d5fa27f
Matthew Horoszowski <Matthew.Horoszowski@gmail.com>
2012-06-26 23:36:15 -0400

    Uploading Prestige Works!

--- src/MindsEyeSociety/LibraryBundle/Controller/DefaultController.php
+++ src/MindsEyeSociety/LibraryBundle/Controller/DefaultController.php
@@ -11,6 +11,7 @@ use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\HttpFoundation\Request;
+use MindsEyeSociety\LibraryBundle\Entity\Award;
 
 class DefaultController extends Controller
 {
@@ -33,21 +34,51 @@ class DefaultController extends Controller
     public function uploadAction(Request $request)
     {
         $collectionConstraint = new Collection(array(
-            'date' => new Date(),
-            'awards' => array(new File(), new Regex(array('pattern' => '/csv/'))),
+            'awards' => array(new File()),
         ));
 
         $form = $this->createFormBuilder(null, array('validation_constraint' => $collectionConstraint))
-            ->add('date', 'date')
             ->add('awards', 'file')
             ->getForm();
-var_export($request->files);
         if ($request->getMethod() == 'POST') {
+            $numberOfAwardsSaved = 0;
             $form->bindRequest($request);
             if ($form->isValid()) {
-                echo "Here";
-                die;
+                $formData = $form->getData();
+
+                if (($handle = fopen($formData['awards']->getPathname(), 'r')) !== FALSE) {
+                    while (($data = fgetcsv($handle)) !== FALSE) {
+                        try {
+                            $award = new Award();
+                            $award->setAwardedDate(new \DateTime($data[0], new \DateTimeZone('America/New_York')));
+                            $award->setEarnedDate(new \DateTime($data[1], new \DateTimeZone('America/New_York')));
+                            $award->setMemberNumber($data[2]);
+                            $award->setCategory($data[3]);
+                            $award->setDescription($data[4]);
+                            $award->setAmount($data[5]);
+                            $award->setType($data[6]);
+                            $award->setNote($data[7]);
+                            $validator = $this->get('validator');
+                            $errors = $validator->validate($award);
+
+                            if ($errors->count() == 0) {
+                                $em = $this->getDoctrine()->getEntityManager();
+                                $em->persist($award);
+                                $em->flush();
+
+                                $numberOfAwardsSaved++;
+                            }
+
+                        } catch (\Exception $e) {
+
+                        }
+                    }
+                    fclose($handle);
+                }
             }
+
+            echo "Upload $numberOfAwardsSaved Awards";
+            die;
         }
 
         return $this->render("MindsEyeSocietyLibraryBundle:Default:uploadForm.html.twig", array('form' => $form->createView()));
